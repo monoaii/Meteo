5bfad08739c32685c2d60f854cb1d735b6445322
MonoAii <141534913+monoaii@users.noreply.github.com>
2023-12-14 01:53:42 -0800

    Injecting weather icon base on api

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ function updateWeatherData(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
-  console.log(response.data);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -19,6 +18,9 @@ function updateWeatherData(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   conditionDescriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url}"
+      class="weather-app-icon"/>`;
 }
 
 //function for date
