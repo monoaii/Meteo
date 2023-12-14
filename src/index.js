213765f31719d290cfd9d8425394ab67ee1e0e28
MonoAii <141534913+monoaii@users.noreply.github.com>
2023-12-13 17:05:10 -0800

    Added search function api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+//update weather temperature to realtime temperature
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#weather-app-temp-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+//make api call and update temperature interface
+function searchCity(city) {
+  let apiKey = "7feob78d78cbdf431afd487b07c045tb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
+//change city to the search city
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Seattle");
